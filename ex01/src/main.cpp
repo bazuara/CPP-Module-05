1da2848647e4bcf61174136e317a46fe28bf8f93
Bruno Azuara <bruno.azuara@gmail.com>
2024-05-25 12:21:07 +0200

    add whole tests

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bazuara <bazuara@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 06:50:12 by bazuara           #+#    #+#             */
-/*   Updated: 2024/05/25 11:38:30 by bazuara          ###   ########.fr       */
+/*   Updated: 2024/05/25 12:11:13 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 #include <Form.hpp>
 
 int main(void) {
+  std::cout << std::endl
+            << "******** Ex00 begins here: *********" << std::endl
+            << std::endl;
   Bureaucrat hermes = Bureaucrat("Hermes Conrad", 150);
   Bureaucrat fry = Bureaucrat("Philip J. Fry", 1);
 
@@ -70,10 +73,12 @@ int main(void) {
     std::cout << e.what() << std::endl;
   }
 
-  std::cout << std::endl << "Ex01 begins here: " << std::endl << std::endl;
+  std::cout << std::endl
+            << "******** Ex01 begins here: *********" << std::endl
+            << std::endl;
 
   Form f1 = Form("Form 1", 1, 1);
-  Form f2 = Form("Form 2", 150, 150);
+  Form f2 = Form("Form 2", 140, 140);
 
   std::cout << f1 << std::endl;
   std::cout << f2 << std::endl;
@@ -81,34 +86,72 @@ int main(void) {
   std::cout << std::endl
             << "Trying to create forms with grades out of bounds:" << std::endl;
   try {
+    std::cout << std::endl << "------ Test ------" << std::endl;
     Form f3 = Form("Form 3", 0, 0);
     std::cout << f3 << std::endl;
   } catch (std::exception& e) {
     std::cout << e.what() << std::endl;
   }
+  std::cout << "--- End of test --" << std::endl << std::endl;
 
   try {
+    std::cout << std::endl << "------ Test ------" << std::endl;
     Form f4 = Form("Form 4", 151, 151);
     std::cout << f4 << std::endl;
   } catch (std::exception& e) {
     std::cout << e.what() << std::endl;
   }
+  std::cout << "--- End of test --" << std::endl << std::endl;
 
   std::cout << std::endl
             << "Trying to sign forms with grades out of bounds:" << std::endl;
   try {
+    std::cout << std::endl << "------ Test ------" << std::endl;
+    std::cout << f1 << std::endl;
+    std::cout << std::endl << fry << std::endl;
     fry.signForm(f1);
   } catch (std::exception& e) {
-    std::cout << e.what() << std::endl;
+    std::cout << e.what();
   }
+  std::cout << "--- End of test --" << std::endl << std::endl;
 
   try {
+    std::cout << std::endl << "------ Test ------" << std::endl;
+    std::cout << f2 << std::endl;
+    std::cout << std::endl << hermes << std::endl;
     hermes.signForm(f2);
   } catch (std::exception& e) {
     std::cout << e.what() << std::endl;
   }
+  std::cout << "--- End of test --" << std::endl << std::endl;
 
   std::cout << std::endl
-            << "Trying to sign forms with proper grades:" << std::endl;
+            << "Trying to sign forms with proper grades from Bureaucrat:"
+            << std::endl;
+  try {
+    std::cout << std::endl << "------ Test ------" << std::endl;
+    fry.incrementGrade();
+    std::cout << f1 << std::endl;
+    std::cout << std::endl << fry << std::endl;
+    fry.signForm(f1);
+    std::cout << f1 << std::endl;
+  } catch (std::exception& e) {
+    std::cout << e.what();
+  }
+  std::cout << "--- End of test --" << std::endl << std::endl;
+
+  std::cout << std::endl
+            << "Trying to sign forms with proper grades from Form:"
+            << std::endl;
+
+  try {
+    std::cout << std::endl << "------ Test ------" << std::endl;
+    std::cout << f2 << std::endl;
+    std::cout << std::endl << fry << std::endl;
+    f2.beSigned(fry);
+    std::cout << f2 << std::endl;
+  } catch (std::exception& e) {
+    std::cout << e.what() << std::endl;
+  }
   return 0;
 }
